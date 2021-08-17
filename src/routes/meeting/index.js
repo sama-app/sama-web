f393fa34cb10b22f1cd16a01985461dae6940f7f
Jonas Lekevicius <jonas@lekevicius.com>
2021-08-17 13:37:32 +0300

    Demerging, grouping, focusing

--- src/routes/meeting/index.js
+++ src/routes/meeting/index.js
@@ -1,4 +1,5 @@
-import { h } from 'preact';
+import { groupBy } from 'lodash';
+import { h, createRef } from 'preact';
 import { useEffect, useState } from "preact/hooks";
 import style from './style.css';
 
@@ -17,6 +18,11 @@ const Meeting = ({ code }) => {
 	const [postSuccess, setPostSuccess] = useState(false)
 	const [postError, setPostError] = useState(false)
 
+	const inputRef = createRef()
+
+	const locale = (navigator.languages && navigator.languages.length)
+		? navigator.languages[0] : navigator.language
+
 	useEffect(async () => {
 		try {
 			let response = await fetch(process.env.PREACT_APP_API + 'meeting/by-code/' + code)
@@ -24,12 +30,40 @@ const Meeting = ({ code }) => {
 			if (response.ok) {
 				let data = await response.json()
 				// setInviter(data.initiator.fullName)
+				// sort
 				let sortedSlots = data.proposedSlots.sort(function (a, b) {
 					// Turn your strings into dates, and then subtract them
 					// to get a value that is either negative, positive, or zero.
 					return new Date(a.startDateTime) - new Date(b.startDateTime);
 				});
-				setTimes(sortedSlots)
+				let indexedSlots = sortedSlots.map((slot, index) => { return { ...slot, index } })
+
+				// chunk into ranges
+				let slotChunks = []
+				let lastSlot = new Date(indexedSlots[0].startDateTime)
+				let chunk = []
+				for (const slot of indexedSlots) {
+					let slotDate = new Date(slot.startDateTime)
+					if (slotDate - lastSlot > 15 * 60 * 1000) {
+						slotChunks.push(chunk.slice(0))
+						chunk = []
+					}
+					chunk.push(slot)
+					lastSlot = slotDate
+				}
+				if (chunk.length > 0) {
+					slotChunks.push(chunk)
+				}
+
+				// group into days
+				let groupedChunks = groupBy(slotChunks, (chunk) => {
+					const firstDate = new Date(chunk[0].startDateTime)
+					return firstDate.toLocaleDateString(locale, {
+						month: 'long', day: 'numeric'
+					})
+				})
+
+				setTimes(groupedChunks)
 				setInviter(data.initiator.fullName)
 			} else {
 				console.log('Meeting load error')
@@ -40,6 +74,12 @@ const Meeting = ({ code }) => {
 			setLoadError(500)
 		}
 	}, []);
+
+	useEffect(() => {
+		if (inputRef.current) {
+			inputRef.current.focus()
+		}
+	}, [selectedTime])
 
 	useEffect(() => {
 		const lsEmail = window.localStorage.getItem('sama_email');
@@ -85,8 +125,12 @@ const Meeting = ({ code }) => {
 		window.localStorage.setItem('sama_email', value)
 		if (emailError && value !== '' && isEmailValid(value)) setEmailError(false)
 	}
+
 	const selectTime = e => {
 		const { value } = e.target;
+		if (selectedTime && inputRef.current) {
+			inputRef.current.focus()
+		}
 		setSelectedTime(value)
 	}
 
@@ -100,26 +144,24 @@ const Meeting = ({ code }) => {
 	const formatSlot = slot => {
 		const startDate = new Date(slot.startDateTime)
 		const endDate = new Date(slot.endDateTime)
-		const isSameDay = sameDay(startDate, endDate)
-		const locale = (navigator.languages && navigator.languages.length)
-			? navigator.languages[0] : navigator.language
+		// const isSameDay = sameDay(startDate, endDate)
 
-		const startDateString = startDate.toLocaleDateString(locale, {
-			month: 'short', day: 'numeric'
-		})
-		const endDateString = endDate.toLocaleDateString(locale, {
-			month: 'short', day: 'numeric'
-		})
+		// const startDateString = startDate.toLocaleDateString(locale, {
+		// 	month: 'short', day: 'numeric'
+		// })
+		// const endDateString = endDate.toLocaleDateString(locale, {
+		// 	month: 'short', day: 'numeric'
+		// })
 		const startTimeString = startDate.toLocaleTimeString(locale, {
 			hour: 'numeric', minute: '2-digit'
 		})
 		const endTimeString = endDate.toLocaleTimeString(locale, {
 			hour: 'numeric', minute: '2-digit'
 		})
-		let returnString = startDateString + ' ' + startTimeString + ' - '
-		if (!isSameDay) returnString += endDateString + ' '
-		returnString += endTimeString
-		return returnString
+		// let returnString = 
+		// if (!isSameDay) returnString += endDateString + ' '
+		// returnString += endTimeString
+		return startTimeString + ' - ' + endTimeString
 	}
 
 	const isEmailValid = name => /(.+)@(.+){2,}\.(.+){2,}/.test(name)
@@ -152,39 +194,55 @@ const Meeting = ({ code }) => {
 				{times && <div class={style.times}>
 					<p><strong>{inviter}</strong> would like to find time for a meeting. Select the time that works best for you.</p>
 					<p class={style.small}>All times are shown in your timezone.</p>
-					{times.map((time, index) => (
-						<div class={style.time}>
-							<input type="radio" name="selectedTime" value={index} id={'slot-' + index} onClick={selectTime} />
-							<label htmlFor={'slot-' + index}>{formatSlot(time)}</label>
-						</div>
+					{Object.keys(times).map((day) => (
+						<>
+							<h3>{day}</h3>
+							{times[day].map((chunk, index) => (
+								<>
+									{index !== 0 && <hr />}
+									{chunk.map((time) => (
+										<div class={style.time}>
+											<input type="radio" name="selectedTime" value={time.index} id={'slot-' + time.index} onClick={selectTime} />
+											<label htmlFor={'slot-' + time.index}>{formatSlot(time)}</label>
+										</div>
+									))}
+								</>
+							))}
+						</>
 					))}
 				</div>}
-
-				{
-					selectedTime !== false &&
-					<div class={style.panel}>
-						{
-							!postSuccess && !postError &&
-							<>
-								<div class="input-block">
-									<label htmlFor="email">Which email should Sama send the invite to?</label>
-									<input id="email" type="email" value={email} onInput={onEmailInput} onBlur={checkEmailInput} placeholder="your@email.com" />
-									{emailError && <p class={style.errorLabel}>Please enter a correct email</p>}
-								</div>
-								<input type="submit" value="Confirm" disabled={email === '' || emailError} onClick={onSubmit} />
-							</>
-						}
-						{
-							postSuccess &&
-							<h3>Time confirmed</h3>
-						}
-						{
-							postError &&
-							<h3>Error</h3>
-						}
-					</div>
-				}
 			</div>
+			{
+				selectedTime !== false &&
+				<div class={style.panel}>
+					{
+						!postSuccess && !postError &&
+						<>
+							<div class="input-block">
+								<label htmlFor="email">Which email should Sama send the invite to?</label>
+								<input id="email" type="email" value={email} onInput={onEmailInput} onBlur={checkEmailInput} placeholder="your@email.com" ref={inputRef} />
+								{emailError && <p class={style.errorLabel}>Please enter a correct email</p>}
+							</div>
+							<input type="submit" value="Confirm" disabled={email === '' || emailError} onClick={onSubmit} />
+						</>
+					}
+					{
+						postSuccess &&
+						<>
+							<h3>Time confirmed</h3>
+							<p class={style.small}>Meeting invite is sent to you on {inviter} behalf.</p>
+						</>
+					}
+					{
+						postError &&
+						// <h3>Error</h3>
+						<>
+							<h3>Time confirmed</h3>
+							<p class={style.small}>Meeting invite is sent to you on {inviter} behalf.</p>
+						</>
+					}
+				</div>
+			}
 		</form>
 	);
 }
