075a27d3778d13a1ea703793b12aedf7b495df53
Jonas Lekevicius <jonas@lekevicius.com>
2021-08-17 15:55:50 +0300

    Copy .well-known

--- preact.config.js
+++ preact.config.js
@@ -1,5 +1,16 @@
 import envVars from 'preact-cli-plugin-env-vars';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 export default function (config, env, helpers) {
   envVars(config, env, helpers);
+
+  config.plugins.push(
+    new CopyWebpackPlugin({
+      patterns: [{
+        from: '.well-known',
+        to: '.well-known'
+        // context: path.resolve(__dirname, 'src')
+      }],
+    }),
+  );
 }
